41e85abe590d9a6c8df765c25792b613d2878d0e
Loveday Ikegbulam <lovedayikegbulam515@gmail.com>
2024-01-26 14:43:51 +0100

    export file handler as module

--- fileHandlers.js
+++ fileHandlers.js
@@ -2,11 +2,16 @@ const fs = require("fs");
 const path = require("path");
 
  function readFromFiles(filePath) {
-	fs.readFile(filePath, "utf8", (err, data) => {
+	return fs.readFile(filePath, "utf8", (err, data) => {
 		if (err) {
 			console.log(err);
 		}
+		console.log(data)
 	})
+}
+
+module.exports = {
+	readFromFiles
 }
 
 
